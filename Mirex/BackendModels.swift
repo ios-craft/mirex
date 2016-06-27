c524e86203e9fb30a0041002b3fa26842e33c2ee
Tomek Cejner <tomek@smartrecruiters.com>
2016-06-28 01:34:43 +0200

    Models for backend

---
+++ Mirex/BackendModels.swift
@@ -0,0 +1,61 @@
+//
+//  BackendModels.swift
+//  Mirex
+//
+//  Created by Tomek on 28.06.2016.
+//  Copyright © 2016 Bottega. All rights reserved.
+//
+
+import Foundation
+import Unbox
+
+struct OfferListItem: Unboxable {
+    
+    let id: Int
+    let title: String
+    let year: Int
+    let price: Int
+    let detailsPath: String
+    
+    init(unboxer: Unboxer) {
+        id = unboxer.unbox("id")
+        title = unboxer.unbox("title")
+        year = unboxer.unbox("year")
+        price = unboxer.unbox("price")
+        detailsPath = unboxer.unbox("details")
+    }
+    
+}
+
+struct Offer: Unboxable {
+    let id: Int
+    let title: String
+    let year: Int
+    let price: Int
+    let mileage: Int
+    let text: String
+    let sellerPath: String
+    
+    init(unboxer: Unboxer) {
+        id = unboxer.unbox("id")
+        title = unboxer.unbox("title")
+        year = unboxer.unbox("year")
+        price = unboxer.unbox("price")
+        mileage = unboxer.unbox("mileage")
+        text = unboxer.unbox("desc")
+        sellerPath = unboxer.unbox("seller")
+    }
+    
+}
+
+
+struct Seller: Unboxable {
+    let name: String
+    let rating: Int
+    
+    init(unboxer: Unboxer) {
+        name = unboxer.unbox("name")
+        rating = unboxer.unbox("rating")
+        
+    }
+}
