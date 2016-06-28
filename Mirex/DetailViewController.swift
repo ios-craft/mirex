6ae2d069aea2b004913b848de589264c878bc2c3
Tomek Cejner <tomek@smartrecruiters.com>
2016-06-28 07:12:37 +0200

    Setup VCs

---
+++ Mirex/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Mirex
+//
+//  Created by Tomek on 28.06.2016.
+//  Copyright © 2016 Bottega. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var tview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
